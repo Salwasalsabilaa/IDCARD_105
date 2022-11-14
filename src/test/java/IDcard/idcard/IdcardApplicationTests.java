060b439025c95df31faff061caac7962cf664a1a
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2022-11-15 00:06:52 +0700

    first commit

---
+++ src/test/java/IDcard/idcard/IdcardApplicationTests.java
@@ -0,0 +1,13 @@
+package IDcard.idcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
